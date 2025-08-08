2e68cfd3cffbc957f3a99541034efea9861b6485
ChongWei905 <nemoweich@163.com>
2025-08-08 14:45:09 +0800

    chore: add random access.

--- rust/lance/src/main.rs
+++ rust/lance/src/main.rs
@@ -22,15 +22,34 @@ async fn main() {
     let args: Vec<String> = env::args().collect();
     println!("程序名称: {}", args[0]);
     let uri = args[1].as_str();
-    let overall_start = Instant::now();
-    test_full_scan_from_lance_file(uri).await;
-    let overall_end = Instant::now();
-    println!("{:?}", overall_end - overall_start);
+    let mode = args[2].as_str();
+    if (mode == "randomaccess") {
+        let indices_file = args[3].as_str();
+        let indices_line = std::fs::read_to_string(indices_file).unwrap();
+        let indices = indices_line.trim();
+
+        let uint_vec: Vec<u32> = indices.split(',')
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
+        let overall_start = Instant::now();
+        test_full_scan_from_lance_file(uri, uint_vec).await;
+        let overall_end = Instant::now();
+        println!("{:?}", overall_end - overall_start);
+    }
+    if (mode == "fullscan") {
+        let overall_start = Instant::now();
+        let empty_vec: Vec<u32> = Vec::new();
+        test_full_scan_from_lance_file(uri, empty_vec).await;
+        let overall_end = Instant::now();
+        println!("read time: {:?}", overall_end - overall_start);
+    }
+
     println!("测试完成！");
 }
 
 pub async fn test_full_scan_from_lance_file(
     file_uri_str: &str,
+    take_rows_array: Vec<u32>,
 ) {
 
     let object_params = ObjectStoreParams {
@@ -55,10 +74,16 @@ pub async fn test_full_scan_from_lance_file(
         &LanceCache::no_cache(),
         FileReaderOptions::default(),
     ).await.unwrap();
+    let read_batch_params: ReadBatchParams;
+    if (take_rows_array.len() > 0) {
+        read_batch_params = ReadBatchParams::Indices(UInt32Array::from(take_rows_array));
+    } else {
+        read_batch_params = ReadBatchParams::RangeFull;
+    }
     let batches = tokio::task::spawn_blocking(move || {
         file_reader
             .read_stream_projected_blocking(
-                ReadBatchParams::RangeFull,
+                read_batch_params,
                 8192,
                 Some(ReaderProjection::from_whole_schema(file_reader.schema(), file_reader.metadata().version())),
                 FilterExpression::no_filter(),
@@ -73,10 +98,6 @@ pub async fn test_full_scan_from_lance_file(
         for column in batch.columns() {
             // 访问数组的长度会触发数据加载
             let _len = column.len();
-
-            for i in 0..column.len() {
-                let _ = column.is_valid(i);
-            }
         }
     });
 }
