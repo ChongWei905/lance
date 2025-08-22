2368ad79402912ea0b96912384dbe06e5adda3a8
ChongWei905 <nemoweich@163.com>
2025-08-22 15:33:25 +0800

    chore: add main.rs for testing.

--- rust/lance/src/main.rs
+++ rust/lance/src/main.rs
@@ -4,7 +4,8 @@ use std::time::Instant;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::sync::Arc;
-use arrow_array::UInt32Array;
+use arrow_array::{RecordBatch, RecordBatchIterator, UInt32Array};
+use arrow_select::concat::concat_batches;
 use futures::StreamExt;
 use object_store::path::Path;
 use lance_core::ArrowResult;
@@ -16,35 +17,37 @@ use lance_io::ReadBatchParams;
 use lance_io::scheduler::{ScanScheduler, SchedulerConfig};
 use lance_io::utils::CachedFileSize;
 use rand::{thread_rng, Rng};
-
+use lance::Dataset;
+use lance::dataset::WriteParams;
+use lance_encoding::version::LanceFileVersion;
+use lance_file::v2::writer::{FileWriter, FileWriterOptions};
+use lance_index::scalar::IndexWriter;
 
 #[tokio::main]
 async fn main() {
     println!("开始执行测试...");
     let args: Vec<String> = env::args().collect();
     println!("程序名称: {}", args[0]);
-    let uri = args[1].as_str();
-    let mode = args[2].as_str();
-    if (mode == "randomaccess") {
-        let indices_file = args[3].as_str();
-        let indices_line = std::fs::read_to_string(indices_file).unwrap();
-        let indices = indices_line.trim();
+    let uri = "/Users/weichong/Documents/working_area/mainLance/lance/lance_v2_0.lance";
+    let mode = "fullscan";
+    // let indices_file = args[3].as_str();
+    // let indices_line = std::fs::read_to_string(indices_file).unwrap();
+    // let indices = indices_line.trim();
+    //
+    // let uint_vec: Vec<u32> = indices.split(',')
+    //     .map(|s| s.parse::<u32>().unwrap())
+    //     .collect();
+    let uint_vec: Vec<u32> = generate_random_numbers();
+    let overall_start = Instant::now();
+    test_full_scan_from_lance_file(uri, uint_vec).await;
+    let overall_end = Instant::now();
+    println!("read time: {:?}", overall_end - overall_start);
 
-        let uint_vec: Vec<u32> = indices.split(',')
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect();
-        let overall_start = Instant::now();
-        test_full_scan_from_lance_file(uri, uint_vec).await;
-        let overall_end = Instant::now();
-        println!("read time: {:?}", overall_end - overall_start);
-    }
-    if (mode == "fullscan") {
-        let overall_start = Instant::now();
-        let empty_vec: Vec<u32> = Vec::new();
-        test_full_scan_from_lance_file(uri, empty_vec).await;
-        let overall_end = Instant::now();
-        println!("read time: {:?}", overall_end - overall_start);
-    }
+    let overall_start = Instant::now();
+    let empty_vec: Vec<u32> = Vec::new();
+    test_full_scan_from_lance_file(uri, empty_vec).await;
+    let overall_end = Instant::now();
+    println!("read time: {:?}", overall_end - overall_start);
 
     println!("测试完成！");
 }
@@ -52,8 +55,8 @@ async fn main() {
 fn generate_random_numbers() -> Vec<u32> {
     let mut rng = thread_rng();
     let mut numbers: Vec<u32> =
-        (0..200)
-        .map(|_| rng.gen_range(1..=10000))
+        (0..20000)
+        .map(|_| rng.gen_range(1..=1000000))
         .collect();
     numbers.sort();
     numbers
@@ -87,6 +90,8 @@ pub async fn test_full_scan_from_lance_file(
         &LanceCache::no_cache(),
         FileReaderOptions::default(),
     ).await.unwrap();
+    let metadata = file_reader.metadata();
+    let schema = file_reader.schema();
     let read_batch_params: ReadBatchParams;
     if (take_rows_array.len() > 0) {
         read_batch_params = ReadBatchParams::Indices(UInt32Array::from(take_rows_array));
@@ -102,15 +107,49 @@ pub async fn test_full_scan_from_lance_file(
             FilterExpression::no_filter(),
         ).unwrap();
 
+    let mut batches: Vec<RecordBatch> = Vec::new();
     while let batch_result = arrow_stream.next() {
         match batch_result.await { batch => {
             if (batch.is_none()) {
                 break;
             }
             let b = batch.unwrap().unwrap();
-            let _len = b.num_rows();
+            println!("batch size: {}", b.num_rows());
         }
         }
     }
+    // let write_schema = batches[0].schema();
+    // let batch_refs: Vec<&RecordBatch> = batches.iter().collect();
+    // let merged_batch = concat_batches(&write_schema, batch_refs).unwrap();
+    //
+    // let uri = "./test_lance";
+    //
+    // let mut reader = RecordBatchIterator::new(batches.into_iter().map(Ok), write_schema);
+    // let dataset = Dataset::write(reader, uri, Some(WriteParams::default())).await.unwrap();
+    //
+    // let storage_options: HashMap<String, String> = HashMap::new();
+    // let object_params = ObjectStoreParams {
+    //     storage_options: Some(storage_options),
+    //     ..Default::default()
+    // };
+    // let write_file_uri_str = "./test_file_lance.lance";
+    // let (obj_store, path) = ObjectStore::from_uri_and_params(
+    //     Arc::new(ObjectStoreRegistry::default()),
+    //     &write_file_uri_str,
+    //     &object_params,
+    // ).await.unwrap();
+    // let obj_store = Arc::new(obj_store);
+    // let obj_writer = obj_store.create(&path).await.unwrap();
+    // let mut file_writer = FileWriter::try_new(obj_writer, merged_batch.schema().as_ref().try_into().unwrap(), FileWriterOptions {
+    //     format_version: Some(LanceFileVersion::V2_0),
+    //     ..Default::default()
+    // }).unwrap();
+    // file_writer.write_batch(&merged_batch).await.unwrap();
+    // file_writer.finish().await.unwrap();
+
+
+
+
+
 
 }
